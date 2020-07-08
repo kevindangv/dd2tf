6d9242e317575a61eac490a13f71069fa79ba9b4
kevindangv <dangkevinv@gmail.com>
2020-07-08 15:34:43 +0000

    updating dashboards.go main.go

--- dashboards.go
+++ dashboards.go
@@ -3,40 +3,68 @@
 package main
 
 import (
-	"fmt"
-	"github.com/zorkian/go-datadog-api"
+	"context"
+        "fmt"
+	"os"
+        datadog "github.com/DataDog/datadog-api-client-go/api/v1/datadog"
 )
+
+
 
 type Dashboard struct {
 }
 
-func (d Dashboard) getElement(client datadog.Client, id interface{}) (interface{}, error) {
-	idStr := fmt.Sprintf("%v", id)
-	dash, err := client.GetDashboard(idStr)
-	return dash, err
+func (d Dashboard) getElement(client datadog.APIClient, id interface{}) (interface{}, error) {
+        ctx := context.WithValue(
+                context.Background(),
+                datadog.ContextAPIKeys,
+                map[string]datadog.APIKey{
+                        "apiKeyAuth": {
+                                Key: os.Getenv("DD_CLIENT_API_KEY"),
+                        },
+                        "appKeyAuth": {
+                                Key: os.Getenv("DD_CLIENT_APP_KEY"),
+                        },
+                },
+        )
+        idStr := fmt.Sprintf("%v", id)
+        dash, r, err := client.DashboardsApi.GetDashboard(ctx, idStr).Execute()
+        return dash, err
 }
 
 func (d Dashboard) getAsset() string {
-	return "tmpl/dashboard.tmpl"
+        return "tmpl/dashboard.tmpl"
 }
 
 func (d Dashboard) getName() string {
-	return "dashboards"
+        return "dashboards"
 }
 
 func (d Dashboard) String() string {
-	return d.getName()
+        return d.getName()
 }
 
-func (d Dashboard) getAllElements(client datadog.Client) ([]Item, error) {
-	var ids []Item
-	dashboards, err := client.GetDashboards()
-	if err != nil {
-		return ids, err
-	}
-	for _, elem := range dashboards {
-		fmt.Println("found dashboard", *elem.Id)
-		ids = append(ids, Item{id: *elem.Id, d: Dashboard{}})
-	}
-	return ids, nil
+func (d Dashboard) getAllElements(client datadog.APIClient) ([]Item, error) {
+        var ids []Item
+	ctx := context.WithValue(
+		context.Background(),
+		datadog.ContextAPIKeys,
+		map[string]datadog.APIKey{
+			"apiKeyAuth": {
+				Key: os.Getenv("DD_CLIENT_API_KEY"),
+			},
+			"appKeyAuth": {
+				Key: os.Getenv("DD_CLIENT_APP_KEY"),
+			},
+		},
+	)
+        dashboards, r, err := client.DashboardsApi.ListDashboards(ctx).Execute()
+        if err != nil {
+                return ids, err
+        }
+        for _, elem := range dashboards {
+                fmt.Println("found dashboard", *elem.Id)
+                ids = append(ids, Item{id: *elem.Id, d: Dashboard{}})
+        }
+        return ids, nil
 }
